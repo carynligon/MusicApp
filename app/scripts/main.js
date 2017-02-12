24fdacbe17aa6ae17eb597c97e30bb75bf5829ff
Caryn Ligon <carynligon@gmail.com>
2017-02-11 19:06:18 -0600

    initial page built out and styled

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,16 +2,31 @@ import $ from 'jquery';
 
 import settings from './settings';
 
+let searchForm = document.querySelector('.search-form');
+let input = document.querySelector('.search-query');
+let resultsUL = document.querySelector('.search-results');
+
+function listItems(data) {
+    console.log(data)
+    let results = data.map((artist) => {
+        let image = '/assets/images/placeholder.jpg';
+        if (artist.images[0]) { image = artist.images[0].url }
+        return $(`<li id=${artist.id}>
+            <div class="artist-image" style="background-image: url(${image})"></div>
+            <h4>${artist.name}</h4>
+        </li>`);
+    });
+    $(resultsUL).append(results);
+}
+
 function getArtists(q) {
     $.ajax({
     url: settings.api,
     data: {q},
-    success:(d) => { console.log(d.artists.items) }
+    success:(d) => { listItems(d.artists.items) }
 });
 }
 
-let searchForm = document.querySelector('.search-form');
-let input = document.querySelector('.search-query');
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     getArtists(input.value);
